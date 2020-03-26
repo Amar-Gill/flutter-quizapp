39d611c1a7659fb73b42f07026e34e6ccefa2319
amar <amar.s.gill@outlook.com>
2020-03-26 15:20:45 -0400

    Exploring themes.

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            alignment: Alignment.center,
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.blue,
-                shape: BoxShape.circle,
-                border: Border.all(width: 5),
-                boxShadow: [
-                  BoxShadow(offset: Offset(40, 40), color: Colors.pink),
-                  BoxShadow(offset: Offset(20, 20), color: Colors.yellow),
-                ],
-                gradient: RadialGradient(colors: [Colors.yellow, Colors.pink])),
-          ),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightGreen,
+        textTheme: TextTheme(
+            body1: TextStyle(color: Colors.red, fontSize: 30),
+            headline: TextStyle(color: Colors.blue, fontSize: 70)),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: Theme.of(context).textTheme.headline,
         ),
       ),
     );
