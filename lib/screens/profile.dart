ae9504aac8c4f908d7e85146e01884aebd86fcef
amar <amar.s.gill@outlook.com>
2020-03-27 20:49:17 -0400

    created profile screen. wrote out rest of services modules.

---
+++ lib/screens/profile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../services/services.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final AuthService auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+
+    var user = Provider.of<FirebaseUser>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text(user.displayName),
+      ),
+      body: Center(
+        child: FlatButton(
+          onPressed: () async {
+            await auth.signOut();
+            Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false)
+          },
+          child: Text('logout'),
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
